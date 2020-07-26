69c26bc999d0b76a2147e894b6c3583ad45c4756
Noah Alston <crashlive112@gmail.com>
2020-07-26 02:36:59 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+x_or_o = ["","","","","","","","","",""]
+def display_board(x_or_o)
+  puts " #{[0]} | #{[1]} | #{[2]} "
+  puts "-----------"
+  puts " #{[3]} | #{[4]} | #{[5]} "
+  puts "-----------"
+  puts " #{[6]} | #{[7]} | #{[8]} "
+end
