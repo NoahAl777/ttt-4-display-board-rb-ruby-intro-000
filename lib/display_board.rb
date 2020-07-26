74c3d453a7c78d48be5a1bca52b7e486558b092f
Noah Alston <crashlive112@gmail.com>
2020-07-26 03:04:21 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@ x_or_o = ["","","","","","","","","",""]
 def display_board(x_or_o)
   puts " #{x_or_o[0]} | #{x_or_o[1]} | #{x_or_o[2]} "
   puts "-----------"
-  puts " #{x_or_o[3]} | #{[x_or_o4]} | #{[x_or_o5]} "
+  puts " #{x_or_o[3]} | #{x_or_o[4]} | #{x_or_o[5]} "
   puts "-----------"
   puts " #{x_or_o[6]} | #{x_or_o[7]} | #{x_or_o[8]} "
 end
